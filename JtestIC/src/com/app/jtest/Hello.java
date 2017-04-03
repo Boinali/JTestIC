a3dd0facc9c23fc429cf66c49a9fe1b315dd5693
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-04-03 15:38:49 +0200

    test3

--- JtestIC/src/com/app/jtest/Hello.java
+++ JtestIC/src/com/app/jtest/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		int j = 20, i = 0;
 		  try {
 		    System.out.println(j/i);
-		  } catch (ArithmeticException e) {
+		  } catch (ArithmeticExcepton e) {
 		    //System.out.println(" Division par zéro !");
 		  }
 
