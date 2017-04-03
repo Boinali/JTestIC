98e100e49194dbafc494688f8f9ceb505abaa394
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-04-03 15:36:41 +0200

    test 2

--- JtestIC/src/com/app/jtest/Hello.java
+++ JtestIC/src/com/app/jtest/Hello.java
@@ -4,6 +4,12 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int j = 20, i = 0;
+		  try {
+		    System.out.println(j/i);
+		  } catch (ArithmeticException e) {
+		    //System.out.println(" Division par zéro !");
+		  }
 
 	}
 
